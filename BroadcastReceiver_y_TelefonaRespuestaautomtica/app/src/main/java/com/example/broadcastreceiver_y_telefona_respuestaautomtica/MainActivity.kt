d0db32d3fa92fead7b5e30cfcce45c2d4afcc8ab
Manuel <perezmane07@gmail.com>
2024-03-09 21:25:48 -0600

    Avance: Implementación de la pantalla principal y de los permisos para el envio de mensajes así como el de llamadas telefónicas.

--- BroadcastReceiver_y_TelefonaRespuestaautomtica/app/src/main/java/com/example/broadcastreceiver_y_telefona_respuestaautomtica/MainActivity.kt
+++ BroadcastReceiver_y_TelefonaRespuestaautomtica/app/src/main/java/com/example/broadcastreceiver_y_telefona_respuestaautomtica/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.broadcastreceiver_y_telefona_respuestaautomtica
 
+import android.app.role.RoleManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.example.broadcastreceiver_y_telefona_respuestaautomtica.ui.theme.BroadcastReceiver_y_TelefoníaRespuestaAutomáticaTheme
 
 class MainActivity : ComponentActivity() {
+    private val REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +38,74 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BrodcastMainScreen()
                 }
+            }
+        }
+        // Solicitar el rol de detección de llamadas
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            val roleManager = getSystemService(RoleManager::class.java)
+            if (!roleManager.isRoleHeld(RoleManager.ROLE_CALL_SCREENING)) {
+                val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
+                startActivityForResult(intent, REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+            } else {
+                finish()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BrodcastMainScreen() {
+    var numero by remember { mutableStateOf(TextFieldValue("")) }
+    var mensaje by remember { mutableStateOf(TextFieldValue("")) }
+    val context = LocalContext.current
+    val sharedPref = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BroadcastReceiver_y_TelefoníaRespuestaAutomáticaTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = numero,
+            onValueChange = { numero = it },
+            label = { Text("Responder Number") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        OutlinedTextField(
+            value = mensaje,
+            onValueChange = { mensaje = it },
+            label = { Text("SMS Text") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        LaunchedEffect(numero, mensaje) {
+            with(sharedPref.edit()) {
+                putString("destinationAddress", numero.text)
+                putString("textMessage", mensaje.text)
+                apply()
+            }
+        }
+
+        Button(
+            onClick = { /* No hacer nada */ },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text("Aceptar")
+        }
     }
 }
